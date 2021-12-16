5514a905d93219fd6e65da5551dae9b7e68fa4b4
Nicolas <v.nicolasvanhuysse@free.fr>
2021-12-16 11:41:36 +0100

    CRUD Salarié

--- ProjetAnnuaire/Controllers/HomeController.cs
+++ ProjetAnnuaire/Controllers/HomeController.cs
@@ -19,6 +19,7 @@ namespace ProjetAnnuaire.Controllers
             _logger = logger;
         }
 
+        // Page Accueil avec la liste des Salariés
         public IActionResult Index(string condition = "")
         {   
             ViewBag.Employee = Employee.GetEmployees();
@@ -34,6 +35,7 @@ namespace ProjetAnnuaire.Controllers
             }
         }
 
+        // Filtre Liste des Salariés
         public IActionResult SubmitForm(string search = null, int idSite = 0, int idService = 0)
         {   
             string condition = "";
@@ -60,20 +62,68 @@ namespace ProjetAnnuaire.Controllers
 
             return RedirectToAction("Index" ,new { @condition = condition });
         }
+        // Formulaire Ajout Salarié
+        public IActionResult SubmitAddForm(Employee e, Site site, Service service)
+        {
+            e.Site = site;
+            e.Service = service;
+            e.Save();
+            return RedirectToAction("Employees");
+        }        
+        
+        // Formulaire Modification Salarié
+        public IActionResult SubmitEditForm(Employee e, Site site, Service service)
+        {
+            e.Site = site;
+            e.Service = service;
+            e.Update();
+            return RedirectToAction("Employees");
+        }
 
-        public IActionResult Privacy()
+        // Delete Salarié
+        public IActionResult DeleteEmployee(int id)
+        {
+            Employee employee = Employee.GetEmployee(id);
+            if (employee != null)
+            {
+                employee.Delete();
+            }
+            return RedirectToAction("Employees");
+        }
+
+        // Vue pour ajouter et modifier un salarié
+        public IActionResult AddEmployee(int id)
+        {
+            ViewBag.Site = Site.GetSites();
+            ViewBag.Service = Service.GetServices();
+            if (id > 0)
+            {
+                ViewBag.Employee = Employee.GetEmployee(id);
+            }
+            return View();
+        }
+
+        // Page Admin
+        public IActionResult Admin()
         {
             return View();
         }
 
+        // Vue pour 1 seul salarié
         public IActionResult ShowEmployee(int id)
         {
             return View(Employee.GetEmployee(id));
         }
 
+        // Vue gestion des Salariés en liste
         public IActionResult Employees()
         {
             return View(Employee.GetEmployees());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
