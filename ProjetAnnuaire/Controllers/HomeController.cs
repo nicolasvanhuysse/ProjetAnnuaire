da898cab4bbe2e51d42b0472b528b2bac724b8b7
Nicolas <v.nicolasvanhuysse@free.fr>
2021-12-16 14:13:32 +0100

    Crud Service

--- ProjetAnnuaire/Controllers/HomeController.cs
+++ ProjetAnnuaire/Controllers/HomeController.cs
@@ -62,63 +62,11 @@ namespace ProjetAnnuaire.Controllers
 
             return RedirectToAction("Index" ,new { @condition = condition });
         }
-        // Formulaire Ajout Salarié
-        public IActionResult SubmitAddForm(Employee e, Site site, Service service)
-        {
-            e.Site = site;
-            e.Service = service;
-            e.Save();
-            return RedirectToAction("Employees");
-        }        
-        
-        // Formulaire Modification Salarié
-        public IActionResult SubmitEditForm(Employee e, Site site, Service service)
-        {
-            e.Site = site;
-            e.Service = service;
-            e.Update();
-            return RedirectToAction("Employees");
-        }
-
-        // Delete Salarié
-        public IActionResult DeleteEmployee(int id)
-        {
-            Employee employee = Employee.GetEmployee(id);
-            if (employee != null)
-            {
-                employee.Delete();
-            }
-            return RedirectToAction("Employees");
-        }
-
-        // Vue pour ajouter et modifier un salarié
-        public IActionResult AddEmployee(int id)
-        {
-            ViewBag.Site = Site.GetSites();
-            ViewBag.Service = Service.GetServices();
-            if (id > 0)
-            {
-                ViewBag.Employee = Employee.GetEmployee(id);
-            }
-            return View();
-        }
-
-        // Page Admin
-        public IActionResult Admin()
-        {
-            return View();
-        }
 
         // Vue pour 1 seul salarié
         public IActionResult ShowEmployee(int id)
         {
             return View(Employee.GetEmployee(id));
-        }
-
-        // Vue gestion des Salariés en liste
-        public IActionResult Employees()
-        {
-            return View(Employee.GetEmployees());
         }
 
         public IActionResult Privacy()
